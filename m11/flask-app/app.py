65d5913bede0db1237ff438beba494f9db2bb8c8
Tarabanov Serhii <tsv198250@gmail.com>
2021-06-03 15:38:28 +0300

    m11

---
+++ m11/flask-app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+# list of cat images
+images = [
+  "https://media.giphy.com/media/lJNoBCvQYp7nq/giphy.gif",
+  "https://media.giphy.com/media/13CoXDiaCcCoyk/giphy.gif",
+  "https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif"
+  ]
+@app.route('/')
+def index():
+    url = random.choice(images)
+    return render_template('index.html',url=url)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
